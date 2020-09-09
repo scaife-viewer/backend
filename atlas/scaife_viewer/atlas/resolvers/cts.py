fb0320392d10c31e8a2e93a9c8f1defdc74abb12
Jacob Wegner <jacobwegner@gmail.com>
2020-09-09 00:12:24 -0500

    move extraction methods out to collection objs

--- atlas/scaife_viewer/atlas/resolvers/cts.py
+++ atlas/scaife_viewer/atlas/resolvers/cts.py
@@ -1,13 +1,4 @@
-import re
-
 from .common import Library
-
-
-def get_lang_value(value):
-    if re.match(r"^[a-z]+-[A-Z][a-z]+$", value):
-        return value.split("-")[0]
-    else:
-        return value
 
 
 class CTSCollectionResolver:
@@ -17,137 +8,23 @@ class CTSCollectionResolver:
         self.versions = {}
         self.resolved = self.resolve_text_inventory(text_inventory)
 
-    def extract_text_group_metadata(self, text_group):
-        """
-            {
-                "urn": "urn:cts:greekLit:tlg0012:",
-                "node_kind": "textgroup",
-                "name": [
-                    {
-                    "lang": "eng",
-                    "value": "Homer"
-                    }
-                ]
-            }
-        """
-        return dict(
-            urn=f"{text_group.urn}:",
-            node_kind="textgroup",
-            name=[dict(lang="eng", value=str(text_group.label))],
-        )
-
-    def extract_work_metadata(self, work):
-        """
-            {
-                "urn": "urn:cts:greekLit:tlg0012.tlg001:",
-                "group_urn": "urn:cts:greekLit:tlg0012:",
-                "node_kind": "work",
-                "lang": "grc",
-                "title": [
-                    {
-                    "lang": "eng",
-                    "value": "Iliad"
-                    }
-                ],
-                "versions": [
-                    {
-                    "urn": "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:",
-                    "node_kind": "version",
-                    "version_kind": "edition",
-                    "first_passage_urn": "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:1.1-1.7",
-                    "citation_scheme": ["book", "line"],
-                    "label": [
-                        {
-                        "lang": "eng",
-                        "value": "Iliad (Greek Text of Munro & Allen)"
-                        }
-                    ],
-                    "description": [
-                        {
-                        "lang": "eng",
-                        "value": "Homer, creator; Monro, D. B. (David Binning), 1836-1905, creator; Monro, D. B. (David Binning), 1836-1905, editor; Allen, Thomas W. (Thomas William), b. 1862, editor"
-                        }
-                    ]
-                    }
-                ]
-            }
-        """
-        return dict(
-            urn=f"{work.urn}:",
-            # @@@
-            group_urn=f'{work.urn.rsplit(".", maxsplit=1)[0]}:',
-            node_kind="work",
-            lang=get_lang_value(work.metadata.lang),
-            # @@@ label vs title wa
-            title=[
-                {
-                    # @@@ hacky
-                    "lang": work.label._language,
-                    "value": str(work.label),
-                }
-            ],
-        )
-
-    def extract_version_metadata(self, version):
-        return dict(
-            urn=f"{version.urn}:",
-            node_kind="version",
-            version_kind=version.kind,
-            # @@@
-            # first_passage_urn
-            citation_scheme=[c.name for c in version.metadata.citation],
-            label=[
-                {
-                    # @@@ hacky
-                    "lang": version.label._language,
-                    "value": str(version.label),
-                }
-            ],
-            description=[
-                {
-                    # @@@ hacky
-                    "lang": version.description._language,
-                    "value": str(version.description),
-                }
-            ],
-            lang=get_lang_value(version.metadata.lang),
-        )
-
     def resolve_versions(self, work):
-        """
-        {
-        "urn": "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:",
-        "node_kind": "version",
-        "version_kind": "edition",
-        "first_passage_urn": "urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:1.1-1.7",
-        "citation_scheme": ["book", "line"],
-        "label": [
-            {
-            "lang": "eng",
-            "value": "Iliad (Greek Text of Munro & Allen)"
-            }
-        ],
-        "description": [
-            {
-            "lang": "eng",
-            "value": "Homer, creator; Monro, D. B. (David Binning), 1836-1905, creator; Monro, D. B. (David Binning), 1836-1905, editor; Allen, Thomas W. (Thomas William), b. 1862, editor"
-            }
-        ]
-        }
-        """
         for version in work.texts():
-            version_metadata = self.extract_version_metadata(version)
+            version_metadata = version.get_atlas_metadata()
+            # version_urn is required within CTSImporter
+            version_urn = version_metadata["urn"]
             # TODO: More validation around "path"
             version_metadata["path"] = None
-            self.versions[version_metadata["urn"]] = version_metadata
+            self.versions[version_urn] = version_metadata
 
     def resolve_works(self, text_group):
         for work in text_group.works():
             if work.urn.count(" ") > 0:
                 # @@@ defensive coding around bad URNs
                 continue
-            work_metadata = self.extract_work_metadata(work)
-            self.works[work_metadata["urn"]] = work_metadata
+            work_metadata = work.get_atlas_metadata()
+            work_urn = work_metadata.pop("urn")
+            self.works[work_urn] = work_metadata
             self.resolve_versions(work)
 
     def resolve_text_inventory(self, text_inventory):
@@ -158,8 +35,9 @@ class CTSCollectionResolver:
         `cts.collections.SORT_OVERRIDES` is respected by ATLAS.
         """
         for text_group in text_inventory.text_groups():
-            text_group_metadata = self.extract_text_group_metadata(text_group)
-            self.text_groups[text_group_metadata["urn"]] = text_group_metadata
+            text_group_metadata = text_group.get_atlas_metadata()
+            tg_urn = text_group_metadata.pop("urn")
+            self.text_groups[tg_urn] = text_group_metadata
             self.resolve_works(text_group)
         return self.text_groups, self.works, self.versions
 
