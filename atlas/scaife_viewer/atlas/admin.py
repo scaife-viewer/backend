229e469c57bf9c735eaafa414d534b0cda3cfc08
Jacob Wegner <jacobwegner@gmail.com>
2020-09-11 10:45:08 -0500

    remove admin views for alignments
    not as useful in a GraphQL world

--- atlas/scaife_viewer/atlas/admin.py
+++ atlas/scaife_viewer/atlas/admin.py
@@ -3,29 +3,9 @@ from django.contrib import admin
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 
-from .models import Node, TextAlignment, TextAlignmentRecord
+from .models import Node
 
 
 @admin.register(Node)
 class NodeAdmin(TreeAdmin):
     form = movenodeform_factory(Node)
-
-
-# @@@ re-introduce version and passage filters
-@admin.register(TextAlignment)
-class TextAlignmentAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "slug", "metadata")
-    raw_id_fields = ["versions"]
-    search_fields = ("name", "slug")
-    prepopulated_fields = {"slug": ["name"]}
-
-
-@admin.register(TextAlignmentRecord)
-class TextAlignmentRecordAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "citation",
-        "idx",
-        "alignment",
-    )
-    list_filter = ("alignment",)
