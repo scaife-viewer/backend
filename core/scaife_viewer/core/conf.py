a2bdbd63fbc6e4574da77bf769bd6d4864077c74
Jacob Wegner <jacobwegner@gmail.com>
2020-10-13 10:22:34 -0500

    customize sorting of collections via a hookset
    - add a hookset that can be customized at the site level
    - add sort_<collection_name> hooks to hookset
    - use existing sort logic as defaults within the hookset
    - standardize sorting so that we're always operating on an iterable of collection objects (rather than sometimes URNs or sometimes objs)

--- core/scaife_viewer/core/conf.py
+++ core/scaife_viewer/core/conf.py
@@ -1,10 +1,35 @@
+import importlib
+
 from django.conf import settings  # noqa
+from django.core.exceptions import ImproperlyConfigured
 
 from appconf import AppConf
+
+
+def load_path_attr(path):
+    i = path.rfind(".")
+    module, attr = path[:i], path[i + 1 :]
+    try:
+        mod = importlib.import_module(module)
+    except ImportError as e:
+        raise ImproperlyConfigured("Error importing {0}: '{1}'".format(module, e))
+    try:
+        attr = getattr(mod, attr)
+    except AttributeError:
+        raise ImproperlyConfigured(
+            "Module '{0}' does not define a '{1}'".format(module, attr)
+        )
+    return attr
 
 
 class CoreAppConf(AppConf):
     ALLOW_TRAILING_COLON = False
 
+    # Other
+    HOOKSET = "scaife_viewer.core.hooks.DefaultHookSet"
+
     class Meta:
         prefix = "scaife_viewer_core"
+
+    def configure_hookset(self, value):
+        return load_path_attr(value)()
