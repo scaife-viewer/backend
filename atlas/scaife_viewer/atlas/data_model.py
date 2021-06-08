8eca839eec128fe9a563a78223f19b50297eaef0
Jacob Wegner <jacobwegner@gmail.com>
2021-06-08 16:08:41 -0500

    Crush metadata migrations into a single migration file

--- atlas/scaife_viewer/atlas/data_model.py
+++ atlas/scaife_viewer/atlas/data_model.py
@@ -12,4 +12,4 @@ content should be re-ingested due to BI schema changes, e.g.:
 * Leveraging the `prepare_atlas_db` management command
 * Comparing a site-level setting to the current VERSION constant
 """
-VERSION = base64.b64encode(b"2021-05-18-001\n").decode()
+VERSION = base64.b64encode(b"2021-06-08-001\n").decode()
