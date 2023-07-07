8ca4c2e2328a7deaeb41fa3f388943d1a4c24328
Jacob Wegner <jacobwegner@gmail.com>
2023-07-07 13:32:52 -0500

    feat: add diff-corproa-contents comand to CLI

--- core/scaife_viewer/core/cli.py
+++ core/scaife_viewer/core/cli.py
@@ -1,6 +1,12 @@
+import json
+import os
+import shlex
+import subprocess
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 import click
+import requests
 
 import ruamel.yaml as yaml
 from github import Github, UnknownObjectException
@@ -70,6 +76,57 @@ def cli():
     """
     Commands used to help develop and manage Scaife Viewer projects
     """
+
+
+@cli.command()
+@click.option(
+    "--new-url",
+    default="https://scaife-dev.perseus.org/corpora/corpus-metadata/",
+    show_default=True,
+)
+@click.option(
+    "--old-url",
+    default="https://scaife.perseus.org/corpora/corpus-metadata/",
+    show_default=True,
+)
+def diff_corpora_contents(new_url, old_url):
+    """
+    Diff two versions of the corpus-metadata manifest.
+
+    If changes are detected, returns a diff to stdout.
+    e.g. scaife diff-corproa-contents > diff.patch
+
+    Returns error code 1 if the manifests are equal.
+
+    """
+    new_data = requests.get(new_url).json()
+    old_data = requests.get(old_url).json()
+    old_data = sorted(old_data, key=lambda x: x.get("repo"))
+    new_data = sorted(new_data, key=lambda x: x.get("repo"))
+
+    old_f = NamedTemporaryFile(suffix="old", mode="w+", delete=False)
+    new_f = NamedTemporaryFile(suffix="new", mode="w+", delete=False)
+
+    try:
+        json.dump(old_data, old_f, indent=2, ensure_ascii=False, sort_keys=True)
+        old_f.close()
+
+        json.dump(new_data, new_f, indent=2, ensure_ascii=False, sort_keys=True)
+        new_f.close()
+
+        command_string = f"diff -U3 {old_f.name} {new_f.name}"
+
+        result = subprocess.run(shlex.split(command_string), capture_output=True)
+        if result.returncode == 0:
+            raise click.ClickException("No content changes detected")
+
+        content = result.stdout.decode("utf-8")
+        content = content.replace(old_f.name, "old.json")
+        content = content.replace(new_f.name, "new.json")
+        click.echo(content)
+    finally:
+        os.unlink(new_f.name)
+        os.unlink(old_f.name)
 
 
 @cli.command()
