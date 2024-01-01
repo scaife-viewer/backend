cb2035dba2186adbf043beebee3c347ac2778beb
Jacob Wegner <jacobwegner@gmail.com>
2024-01-01 16:58:26 -0600

    Add debugging

--- cts_utils/devops/convert.sh
+++ cts_utils/devops/convert.sh
@@ -5,4 +5,5 @@ docker run --rm \
     -v $1:/opt/cts-utils/data \
     cts-utils:latest \
     bash -c "python tests/conversion.py /opt/cts-utils/data/cts-templates && \
-    python tests/copy_files.py /opt/cts-utils/data/cts-templates"
+    python tests/copy_files.py /opt/cts-utils/data/cts-templates && \
+    ls -la /opt/cts-utils/data"
